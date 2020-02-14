4a60066da3aa376d4ca43a6db3d40e52487bf5d1
Fernanda Carvalho <fernanda.carvalho@radixeng.com.br>
2020-02-14 11:03:57 -0300

    Adding editingBegin animation

--- FCTextField/FCTextField/FCTextField.swift
+++ FCTextField/FCTextField/FCTextField.swift
@@ -20,18 +20,58 @@ public class FCTextField: UITextField {
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
+        self.initView()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        self.initView()
     }
+    
+    //MARK: SETUPS
     
     fileprivate func initView() {
         self.borderStyle = .none
         self.layer.borderWidth = 0
-        self.bottomLine.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: 1)
+        self.bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1 , width: self.frame.size.width, height: 1)
         self.bottomLine.backgroundColor = self.bottomLineColor
         self.addSubview(self.bottomLine)
+        self.configureEvents()
     }
     
+    fileprivate func configureEvents() {
+        addTarget(self, action: #selector(self.didBeginEditing), for: .editingDidBegin)
+    }
+    
+    //MARK: HANDLERS
+    @objc fileprivate func didBeginEditing() {
+        self.hideBottomLine()
+    }
+    
+    //MARK: ANIMATIONS
+    fileprivate func hideBottomLine() {
+        UIView.animate(withDuration: 0.8, animations: {
+            self.bottomLine.frame.size.width = 0
+            self.bottomLine.center.x = self.frame.width/2
+        }) { (true) in
+            self.bottomLine.isHidden = true
+            self.showBottomLine()
+        }
+    }
+    
+    fileprivate func showBottomLine() {
+        UIView.animate(withDuration: 0.8, animations: {
+            self.bottomLine.center.x = 0
+            self.bottomLine.frame.size.width = self.frame.size.width
+            self.bottomLine.isHidden = false
+        }) { (true) in
+            self.changeToActiveColor()
+        }
+    }
+    
+    fileprivate func changeToActiveColor() {
+        UIView.animate(withDuration: 0.8, animations: {
+            self.bottomLine.backgroundColor = .purple
+        })
+    }
 }
