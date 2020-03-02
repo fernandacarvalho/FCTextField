e790bc5511624bb1e1b5a0c830ccc5c6ccd7997e
Fernanda Carvalho <fernanda.carvalho@radixeng.com.br>
2020-03-02 16:37:46 -0300

    DidEndEditing animations and custom clear button

--- FCTextField/FCTextField/FCTextField.swift
+++ FCTextField/FCTextField/FCTextField.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
+@IBDesignable 
 public class FCTextField: UITextField {
     
     fileprivate var bottomLine = UIView()
+    fileprivate var tintedClearImage: UIImage?
     
-    open var bottomLineColor : UIColor = .black {
+    @IBInspectable open var bottomDefaultColor : UIColor = .lightGray {
         didSet {
-            self.bottomLine.backgroundColor = self.bottomLineColor
+            self.bottomLine.backgroundColor = self.bottomDefaultColor
         }
     }
+    
+    @IBInspectable open var bottomActiveColor : UIColor = .darkGray
+    
+    @IBInspectable open var placeholderColor : UIColor = .lightGray {
+        didSet {
+            self.attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor:placeholderColor])
+        }
+    }
+    
+    
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,16 +43,44 @@ public class FCTextField: UITextField {
     //MARK: SETUPS
     
     fileprivate func initView() {
-        self.borderStyle = .none
-        self.layer.borderWidth = 0
-        self.bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1 , width: self.frame.size.width, height: 1)
-        self.bottomLine.backgroundColor = self.bottomLineColor
-        self.addSubview(self.bottomLine)
+        self.customizeBorder()
+        self.customizePlaceholder()
         self.configureEvents()
     }
     
     fileprivate func configureEvents() {
         addTarget(self, action: #selector(self.didBeginEditing), for: .editingDidBegin)
+        addTarget(self, action: #selector(self.didEndEditing), for: .editingDidEnd)
+    }
+    
+    fileprivate func customizePlaceholder() {
+        self.attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor:placeholderColor])
+    }
+    
+    fileprivate func customizeBorder() {
+        self.borderStyle = .none
+        self.layer.borderWidth = 0
+        
+        self.bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1 , width: self.frame.size.width, height: 1)
+        self.bottomLine.backgroundColor = self.bottomDefaultColor
+        self.addSubview(self.bottomLine)
+    }
+    
+ 
+    fileprivate func customizeClearButton() {
+        for view in self.subviews {
+            if view is UIButton {
+                let button = view as! UIButton
+                if let image = button.image(for: .highlighted) {
+                    if self.tintedClearImage == nil {
+                        tintedClearImage = image.withTintColor(self.bottomActiveColor)
+                    }
+                    button.setImage(self.tintedClearImage, for: .normal)
+                    button.setImage(self.tintedClearImage, for: .highlighted)
+                    break
+                }
+            }
+        }
     }
     
     //MARK: HANDLERS
@@ -48,9 +88,21 @@ public class FCTextField: UITextField {
         self.hideBottomLine()
     }
     
+    @objc fileprivate func didEndEditing() {
+        self.backToDefaultColor()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        if self.isFirstResponder {
+            self.customizeClearButton()
+        }
+    }
+    
+    
     //MARK: ANIMATIONS
     fileprivate func hideBottomLine() {
-        UIView.animate(withDuration: 0.8, animations: {
+        UIView.animate(withDuration: 0.5, animations: {
             self.bottomLine.frame.size.width = 0
             self.bottomLine.center.x = self.frame.width/2
         }) { (true) in
@@ -60,7 +112,7 @@ public class FCTextField: UITextField {
     }
     
     fileprivate func showBottomLine() {
-        UIView.animate(withDuration: 0.8, animations: {
+        UIView.animate(withDuration: 0.5, animations: {
             self.bottomLine.center.x = 0
             self.bottomLine.frame.size.width = self.frame.size.width
             self.bottomLine.isHidden = false
@@ -70,8 +122,15 @@ public class FCTextField: UITextField {
     }
     
     fileprivate func changeToActiveColor() {
-        UIView.animate(withDuration: 0.8, animations: {
-            self.bottomLine.backgroundColor = .purple
+        UIView.animate(withDuration: 0.2, animations: {
+            self.bottomLine.backgroundColor = self.bottomActiveColor
+        })
+    }
+    
+    fileprivate func backToDefaultColor() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.bottomLine.backgroundColor = self.bottomDefaultColor
         })
     }
 }
+
