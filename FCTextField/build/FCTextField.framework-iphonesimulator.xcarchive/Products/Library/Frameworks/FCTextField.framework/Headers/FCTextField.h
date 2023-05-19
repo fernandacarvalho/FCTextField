9a6fff5828a2488a3c003682e8fff44ac3438a2b
Fernanda Carvalho <fernanda.carvalho@mobicare.com.br>
2023-05-18 23:24:39 -0300

    Exportando framework

---
+++ FCTextField/build/FCTextField.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/FCTextField.framework/Headers/FCTextField.h
@@ -0,0 +1,18 @@
+//
+//  FCTextField.h
+//  FCTextField
+//
+//  Created by Fernanda FC. Carvalho on 17/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FCTextField.
+FOUNDATION_EXPORT double FCTextFieldVersionNumber;
+
+//! Project version string for FCTextField.
+FOUNDATION_EXPORT const unsigned char FCTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCTextField/PublicHeader.h>
+
+
